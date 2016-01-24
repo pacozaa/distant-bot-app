4770d879c9274e81b3b8ee4a60cc18a463566940
pacozaa <pacosarin@gmail.com>
2016-01-24 16:34:48 +0700

    initialize

---
+++ prototype/index.js
@@ -0,0 +1,17 @@
+var app = angular.module('StarterApp', ['ngMaterial','ngMdIcons']);
+
+app.controller('AppController', function() {
+
+});
+function initialize() {
+  var mapProp = {
+    center:new google.maps.LatLng(51.508742,-0.120850),
+    zoom:5,
+    mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+  var map=new google.maps.Map(document.getElementById("gmap"), mapProp);
+
+   google.maps.event.trigger(map, "resize");
+
+}
+google.maps.event.addDomListener(window, 'load', initialize);
