f100168474f2f7b5ccacdf373aea80af85d77410
pacozaa <pacosarin@gmail.com>
2016-02-07 21:10:27 +0700

    use npm jimp

--- webapp/distant-bot-app/server/rtsp.js
+++ webapp/distant-bot-app/server/rtsp.js
@@ -1,12 +1,15 @@
 const rtsp = Meteor.npmRequire('rtsp-ffmpeg');
-//const getPixels  = Meteor.npmRequire('pixel-getter');
+const getPixels  = Meteor.npmRequire('pixel-getter');
+const convert  = Meteor.npmRequire('color-convert');
+const Jimp = Meteor.npmRequire('jimp');
 const RobotCameraInfo = {
   IP: 'udp://localhost:1234/1234'
 }
 const RobotCamera = new rtsp.FFMpeg({
   input: RobotCameraInfo.IP,
   resolution: '320x240',
-  quality: 3
+  quality: 3,
+  rate: 5
 });
 RobotCamera.on('start', () => {
   console.log('RobotCamera : start');
@@ -21,12 +24,18 @@ Streamy.onConnect((fromSocket) => {
   console.log('onConnect');
   RobotCamera.on('data', streamToClient = (bytes) => {
     Streamy.emit('VideoData', {data: bytes}, fromSocket);
+    Jimp.read(bytes, function (err, image) {
+        if (err){
+          console.log(err);
+        }
+        console.log(Jimp.intToRGBA(image.getPixelColor(50, 50)));
+    });
     // getPixels.get(bytes, function(err, pixels) {
     //   if(err){
-    //     console.log('got shit');
+    //     console.log(err);
     //   }
     //   else {
-    //     console.log(pixels[0][0]);
+    //     console.log(pixels[0].length);
     //   }
     // });
   });
